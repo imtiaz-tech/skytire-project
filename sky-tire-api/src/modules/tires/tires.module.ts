43ca7fdbd16a46564df6b309998bf5b071c99714
imtiaz-tech <imtiazahmadswl93@gmail.com>
2026-04-30 00:30:25 +0500

    changes

--- sky-tire-api/src/modules/tires/tires.module.ts
+++ sky-tire-api/src/modules/tires/tires.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TiresService } from './tires.service';
 import { TiresController } from './tires.controller';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [TiresController],
   providers: [TiresService],
 })
